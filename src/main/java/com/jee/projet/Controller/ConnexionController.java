61d9d77b8476755bb743c2096f45239fd5aca3e4
GangneuxA <gangneux.alex@gmail.com>
2023-04-07 10:38:26 +0200

    bug réparé

--- src/main/java/com/jee/projet/Controller/ConnexionController.java
+++ src/main/java/com/jee/projet/Controller/ConnexionController.java
@@ -3,7 +3,7 @@ package com.jee.projet.Controller;
 import com.jee.projet.DAO.UserRepository;
 import com.jee.projet.ENTITY.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,14 +24,5 @@ public class ConnexionController {
         return "inscription";
     }
 
-    @PostMapping("/process_register")
-    public String processRegister(User user) {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String encodedPassword = passwordEncoder.encode(user.getMotDePasse());
-        user.setMotDePasse(encodedPassword);
 
-        userRepo.save(user);
-
-        return "register_success";
-    }
 }
